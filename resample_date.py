b5e924f252d7bcf564be05783bba2e168e321210
shadowtraveler <32443338+shadowtraveler@users.noreply.github.com>
2020-12-07 17:49:35 +0800

    Add files via upload

--- resample_date.py
+++ resample_date.py
@@ -9,21 +9,24 @@ import pandas as pd
 import os
 
 def draw_six(data,start_day='2018-09-25'):
-    data=data.drop_duplicates()
+    data['value']=data['value'].interpolate()
+    data['datetime']=data['datetime'].drop_duplicates()
     timest=data.pop('datetime')
     timest=pd.to_datetime(timest)
     data=data.set_index(timest)
     #data_min=data.resample('1min',origin=start_day).mean()
     data_min=data.resample('1min',convention='start').mean()
+    data_min['value']=data_min['value'].interpolate()
     return data_min
 
 
-def read_and_save(my_path='D:/document/NCTU_sec1/Powermeter/'):
+def read_and_save(my_path='D:/document/NCTU_sec1/Powermeter/20201024data/data/smartMeter/'):
     if(not os.path.isdir(my_path+"/output")):
         os.mkdir(my_path+"/output")
     try:
         Path_o=my_path
         Path_dictionary=['Consumption-O','Current1','Current2','Power1','Power2','Volt1','Volt2',]
+        #Path_dictionary=['Consumption-O']
         dt=pd.DataFrame()
         for p_d in Path_dictionary:
             dt=pd.DataFrame()
@@ -34,13 +37,14 @@ def read_and_save(my_path='D:/document/NCTU_sec1/Powermeter/'):
                 df=pd.read_csv(os.path.join(Path_tmp,file))
                 dt=dt.append(df,ignore_index=True)
             dt=draw_six(dt)
-            dt.rename(columns={'value':'value_'+p_d})
-            dt.to_csv('output/output_'+p_d+".csv")
+            dt=dt.rename(columns={'value':'value_'+p_d})
+            dt.to_csv('output/output_new_'+p_d+".csv")
             print(p_d+" finished.")
     except:
         raise ValueError("There is an error in read_and_save function\nPlease check your path.")
 
 if __name__ == '__main__':
-    my_path='D:/document/NCTU_sec1/Powermeter/' #input the path you put the data where named Powermeter in default.
-    Path_dictionary=['Consumption-O','Current1','Current2','Power1','Power2','Volt1','Volt2','Default(Current2)']
-    my_dt=read_and_save(my_path)
+    #my_path='D:/document/NCTU_sec1/Powermeter/' #input the path you put the data where named Powermeter in default.
+    #Path_dictionary=['Consumption-O','Current1','Current2','Power1','Power2','Volt1','Volt2','Default(Current2)']
+    #my_dt=read_and_save(my_path)
+    read_and_save()
